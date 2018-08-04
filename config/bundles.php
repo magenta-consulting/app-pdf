d4eb6748459eba0a4e394504edce5736374d42ce
Peter Pierre <binh@sunrise.vn>
2018-08-04 19:17:18 +0700

    register in bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Bean\Bundle\DevToolBundle\BeanDevToolBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+    Magenta\Bundle\AppPdfBundle\MagentaAppPdfBundle::class => ['all' => true, 'test' => true],
 ];
