2512a4598df5ba158a92cc2e4e49b54945bcc4f7
Peter Pierre <binh@sunrise.vn>
2018-08-04 19:07:46 +0700

    first commit

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Bean\Bundle\DevToolBundle\BeanDevToolBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
+];
