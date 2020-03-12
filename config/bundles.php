d31c427e3541fede61968cccb7d6390223087fc3
binhle410 <binh@sunrise.vn>
2020-03-12 19:37:46 +0700

    Update Lib

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Bean\Bundle\DevToolBundle\BeanDevToolBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Magenta\Bundle\AppPdfBundle\MagentaAppPdfBundle::class => ['all' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
